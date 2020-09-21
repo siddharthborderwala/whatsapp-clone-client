8c12ffd862e3023f2d2a71a8b9ee41c5921f974d
Siddharth Borderwala <siddharthborderwala@gmail.com>
2020-09-21 19:34:40 +0530

    Rename of component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import ChatListScreen from './components/ChatListScreen';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  RouteComponentProps,
+} from 'react-router-dom';
+
+import MyAnimatedSwitch from './components/AnimatedSwitch';
+
+import ChatsListScreen from './components/ChatsListScreen';
+import ChatRoomScreen from './components/ChatRoomScreen';
+
+const redirectToChats = () => <Redirect to="/chats" />;
 
 const App: React.FC = () => {
   return (
-    <div>
-      <ChatListScreen />
-    </div>
+    <BrowserRouter>
+      <MyAnimatedSwitch>
+        <Route exact path="/chats" component={ChatsListScreen} />
+        <Route
+          exact
+          path="/chat/:chatId"
+          component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} />
+          )}
+        />
+      </MyAnimatedSwitch>
+      <Route exact path="/" render={redirectToChats} />
+    </BrowserRouter>
   );
 };
 
